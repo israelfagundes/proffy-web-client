fd01136b6081e31a8974c92cd10feb3ee896e4a6
Israel <israel.fagundes2001@gmail.com>
2020-08-03 22:34:18 -0300

    add teacher list page

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+const TeacherItem: React.FC = () => {
+  return (
+    <article className="teacher-item">
+      <header>
+        <img
+          src="https://api.adorable.io/avatars/285/atob@adorable.io.png"
+          alt="Avatar"
+        />
+        <div>
+          <strong>Jonas Corujão</strong>
+          <span>Química</span>
+        </div>
+      </header>
+      <p>
+        Entusiasta das melhores tecnologias de química avançada. <br /> <br />
+        Apaixonado por explodir coisas em laboratório e por mudar a vida das
+        pessoas através de experiências. Mais de 200.000 pessoas já passaram por
+        uma das minhas explosões.
+      </p>
+
+      <footer>
+        <p>
+          Preço/hora
+          <strong>R$100,00</strong>
+        </p>
+        <button type="button">
+          <img src={whatsappIcon} alt="WhatsApp" />
+          Entrar em contato
+        </button>
+      </footer>
+    </article>
+  );
+};
+
+export default TeacherItem;
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -1,7 +1,46 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+
+import './styles.css';
+
 function TeacherList() {
-  return <h1>TeacherList</h1>;
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">
+              Matéria
+              <input type="text" id="subject" />
+            </label>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week-day">
+              Dia da semana
+              <input type="text" id="week-day" />
+            </label>
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">
+              Horário
+              <input type="text" id="time" />
+            </label>
+          </div>
+        </form>
+      </PageHeader>
+
+      <main>
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </main>
+    </div>
+  );
 }
 
 export default TeacherList;
